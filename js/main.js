82d76c8d645ffb82e5082c7f558b45f2bc9d7fce
Hoosfive <tolyan145@gmail.com>
2022-11-16 16:10:24 +0300

    Добавлено чтение параметра с городов из url и заполнение его значением выпадающего списка городов + изменения в вёрстке и стилях

--- js/main.js
+++ js/main.js
@@ -1,43 +1,67 @@
-const PROFILE_DATA_PARAM = 'data';
+const PROFILE_DATA_URL_PARAM = 'profile';
+const ALL_CITIES_URL_PARAM = 'allCities';
 
-let inputs = {
+let profileFormElements = {
 	userName: document.querySelector('#userName'),
-	city: document.querySelector('#city'),
+	cityName: document.querySelector('#selectedCity'),
 	position: document.querySelector('#position'),
-	about: document.querySelector('#about'),
+	about: document.querySelector('#about')
 }
-
-document.querySelector('#profileForm').addEventListener('submit', showMainButton)
 
 function showMainButton(event) {
 	Telegram.WebApp.MainButton.show()
-	event.preventDefault(event);
+	event.preventDefault(event)
 }
 
 function sendProfile() {
-	let profile = {
-		userName: inputs.userName.value,
-		city: inputs.city.value,
-		position: inputs.position.value,
-		about: inputs.about.value
+	let profileFormData = {
+		userName: profileFormElements.userName.value,
+		cityName: profileFormElements.cityName.innerText,
+		position: profileFormElements.position.value,
+		about: profileFormElements.about.value
 	}
-	Telegram.WebApp.sendData(JSON.stringify(profile))
+	Telegram.WebApp.sendData(JSON.stringify(profileFormData))
 }
 
-function setUrlParamJsonToInputsValues() {
-	let urlParams = new URLSearchParams(document.location.search);
-	if (urlParams.has(PROFILE_DATA_PARAM)) {
-		let profile = JSON.parse(urlParams.get(PROFILE_DATA_PARAM));
-		for (param in profile) {
-			if (param != null) {
-				inputs[param].value = profile[param];
+function processUrlParams() {
+	let urlParams = new URLSearchParams(document.location.search)
+	if (urlParams.has(PROFILE_DATA_URL_PARAM)) {
+		setProfileParamValuesToFields(urlParams)
+	}
+	if (urlParams.has(ALL_CITIES_URL_PARAM)) {
+		setCitiesListParamToCitiesDropdown(urlParams)
+	}
+}
+
+function setProfileParamValuesToFields(urlParams) {
+	let profile = JSON.parse(urlParams.get(PROFILE_DATA_URL_PARAM))
+	for (param in profile) {
+		if (param != null) {
+			if (param !== 'cityName') {
+				profileFormElements[param].value = profile[param]
+			} else {
+				profileFormElements[param].innerText = profile[param]
 			}
 		}
 	}
 }
+function setCitiesListParamToCitiesDropdown(urlParams) {
+	let allCities = urlParams.get(ALL_CITIES_URL_PARAM).split(",")
+	let btnListElement = document.querySelector('#dropdown-button-list')
+	if (allCities.length > 0 && allCities[0] !== '') {
+		document.querySelectorAll('#cityPlaceholder').forEach(el => el.remove())
+		allCities.forEach(cityName => {
+			let htmlEl = document.createElement("button")
+			htmlEl.className = 'dropdown-item'
+			htmlEl.type = 'button'
+			htmlEl.innerText = cityName
+			btnListElement.appendChild(htmlEl)
+		})
+	}
+}
 
 function init() {
-	setUrlParamJsonToInputsValues()
+	processUrlParams()
 	Telegram.WebApp.ready()
 	Telegram.WebApp.MainButton
 		.setText('Сохранить')
@@ -45,3 +69,4 @@ function init() {
 }
 
 document.addEventListener('DOMContentLoaded', init)
+document.querySelector('#profileForm').addEventListener('submit', showMainButton)
